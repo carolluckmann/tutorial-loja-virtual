b1512ac7011fc7748f1d936790b5abff59696f56
carolluckmann <carolinaluckmann05@gmail.com>
2024-07-02 11:06:54 -0300

    Chamada da API

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.1es1r5va4h8"
+    "revision": "0.17qdmtgcd9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
