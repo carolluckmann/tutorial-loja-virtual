30c891dcdfd12df59930b4abb7d425e1867db99d
carolluckmann <carolinaluckmann05@gmail.com>
2024-07-15 11:49:29 -0300

    Autenticação em PWA

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bkl7osi496"
+    "revision": "0.tfin8eavv38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
-  <h1>Pg de Login</h1>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="XkHUriM4JcCSuk6JvbzFZxSJ"></passage-auth>
+  </div>
 </template>
+
+<style></style>
